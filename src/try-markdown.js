0984890e501f1cace0090eebc36a6842bc042058
Przemyslaw Warchol <warcholprzemo@gmail.com>
2018-12-16 16:00:53 +0100

    Add navigation links to Blog

--- src/try-markdown.js
+++ src/try-markdown.js
@@ -3,6 +3,15 @@ import './try-markdown.css';
 import {Link} from 'react-router-dom';
 const ReactMarkdown = require('react-markdown');
 
+function ShowInstanceLink(props){
+    const instance = props.instance;
+    if(instance){
+        return(
+            <Link to={`/blog/${instance['id']}`} title={ instance.title }>{ instance.title }</Link>
+        )
+    }
+    return (<span>---</span>);
+}
 
 export class BlogDetail extends React.Component{
     constructor(props){
@@ -10,11 +19,19 @@ export class BlogDetail extends React.Component{
         this.state = {
             remote_url: API_URL + '/api/blog/' + this.props.match.params.id + "/",
             blog: {},
+            prev_blog: null,
+            next_blog: null
         }
     }
 
     componentDidMount(){
         this.fillBlogDetail();
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            remote_url: API_URL + '/api/blog/' + nextProps.match.params.id + "/",
+        }, this.fillBlogDetail); //call fillBlogDetail as callback
     }
 
     fillBlogDetail(){
@@ -27,7 +44,9 @@ export class BlogDetail extends React.Component{
         })
         .then(response_json => {
             this.setState({
-                blog: response_json
+                blog: response_json['instance'],
+                prev_blog: response_json['prev_instance'],
+                next_blog: response_json['next_instance']
             });
         });
     }
@@ -36,6 +55,11 @@ export class BlogDetail extends React.Component{
         return(
             <div>
                 <h1>{ this.state.blog.title }</h1>
+                <div>
+                    <span className="navigation-link"><ShowInstanceLink instance={this.state.prev_blog} /></span>
+                    <span className="arrows">&lt;&lt; &gt;&gt;</span>
+                    <span className="navigation-link"><ShowInstanceLink instance={this.state.next_blog} /></span>
+                </div>
                 <div className="try-markdown-main">
                     <ReactMarkdown source={ this.state.blog.content } />
                 </div>
